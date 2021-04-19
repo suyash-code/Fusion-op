aad2978b7f0edbbe761231b8474b3122daa0822e
Suyash Khare <2018257@iiitdmj.ac.in>
2021-04-20 00:00:00 +0530

    Forward Indent

--- FusionIIIT/applications/ps1/urls.py
+++ FusionIIIT/applications/ps1/urls.py
@@ -7,4 +7,7 @@ app_name = 'ps1'
 urlpatterns = [
 
     url(r'^$', views.ps1, name='ps1'),
+    url(r'^inwardIndent/$', views.inward, name='inward'),
+    url(r'^indentview2/(?P<id>\d+)$', views.indentview2, name='indentview2'),
+    url(r'^forwardindent/(?P<id>\d+)/$', views.forwardindent, name='forwardindent'),
 ]
