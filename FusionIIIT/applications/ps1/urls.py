9c0c027745271e30346ec71432d19862dcde93ca
Shubham Rajani <shubhamrajani1999@gmail.com>
2021-04-20 23:00:20 +0530

    Filing Indent,View Composed Indents

--- FusionIIIT/applications/ps1/urls.py
+++ FusionIIIT/applications/ps1/urls.py
@@ -7,6 +7,8 @@ app_name = 'ps1'
 urlpatterns = [
 
     url(r'^$', views.ps1, name='ps1'),
+    url(r'^composed_indents/$', views.composed_indents, name='composed_indents'),
+    url(r'^indentview/(?P<id>\d+)$', views.indentview, name='indentview'),
     url(r'^inwardIndent/$', views.inward, name='inward'),
     url(r'^indentview2/(?P<id>\d+)$', views.indentview2, name='indentview2'),
     url(r'^forwardindent/(?P<id>\d+)/$', views.forwardindent, name='forwardindent'),
